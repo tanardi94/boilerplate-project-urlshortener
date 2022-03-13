aa8fbc7332250d0a006f56b8efd8fee7f3c5ce5e
Rahardian Dewa Bimantara <tanardi94@users.noreply.github.com>
2022-03-13 08:58:03 +0000

    verified by fcc

--- server.js
+++ server.js
@@ -1,76 +1,103 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-const bodyParser = require('body-parser')
-
-const dns = require('dns')
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
 
-const shortNum = generateRandomInteger(100)
-var originalUrl
+const { Schema } = mongoose;
+
+const urlSchema = new Schema({
+  original_url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: String,
+    required: true,
+  },
+});
+
+const Url = mongoose.model('url', urlSchema);
+
+const createAndSaveUrl = (originalUrl) => {
+  return Url.count({})
+    .then((count) => {
+      const urlCheck = new URL(originalUrl);
+      if (urlCheck.code === 'ERR_INVALID_URL') {
+        throw err;
+      }
+
+      if (!originalUrl.startsWith('http') && !originalUrl.startsWith('https')) {
+        throw err;
+      }
+
+      const url = new Url({
+        original_url: originalUrl,
+        short_url: count + 1,
+      });
+
+      return url.save();
+    })
+    .then((savedObj) => {
+      return { original_url: savedObj.original_url, short_url: savedObj.short_url };
+    })
+    .catch((errObj) => {
+      return { error: 'invalid url' };
+    });
+};
+
+const getOriginalUrl = (shortUrl) => {
+  return Url.find({ short_url: shortUrl })
+    .then((urlObj) => {
+      return urlObj[0]['original_url'];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use(bodyParser.urlencoded({
-	extended: true
-}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', (req, res) => {
-
-	originalUrl = req.body.url
-
-	var protocol = originalUrl.split("://")
-
-	const res1 = originalUrl.replace(protocol[0] + '://', '');
-	dns.lookup(res1, (err, address, family) => {
-		var jsons = {
-			original_url: originalUrl,
-			short_url: shortNum
-		}
-
-		if (err) {
-			jsons = {
-				error: 'invalid url'
-			}
-		}
-
-		return res.json(jsons)
-		
-	})
-
-	
-
-	
-})
-
-app.get('/api/shorturl/:num', (req, res) => {
-	res.writeHead(302, {Location: originalUrl})
-	res.end()
-})
-
-
-
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get('/api/shorturl/:url', (req, res) => {
+  const url = req.params.url;
+  getOriginalUrl(url)
+    .then((originalUrl) => {
+      res.redirect(originalUrl);
+    })
+    .catch(() => {
+      res.json({ error: 'No such URL found' });
+    });
 });
 
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  createAndSaveUrl(url)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-function generateRandomInteger(max) {
-    return Math.floor(Math.random() * max) + 1;
-}
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
